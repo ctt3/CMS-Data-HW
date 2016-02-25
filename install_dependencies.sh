bc698953df341a2d0358a157c03b4d09a939241e
Charlie <ctt3>
2016-02-25 16:13:48 -0500

    add jupyter to dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
+# install dependencies using pip
 pip install numpy
 pip install pandas
 pip install bokeh
 pip install ipython
 pip install pokitdok
+pip install jupyter
+
+# reset bash profile
+. ~/.bash_profile
 
 echo "Requirements installed."
